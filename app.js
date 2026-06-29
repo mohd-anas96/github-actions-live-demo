1e185c00c6d8501dc1aae7adc7aac3518e185f6d
EC2 Default User <ec2-user@ip-172-31-8-13.ap-south-1.compute.internal>
2026-06-29 15:08:49 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send(`
+        <h1>GitHub Actions CI/CD Demo</h1>
+        <h2>Version 1.0</h2>
+        <p>Application Successfully Deployed using GitHub Actions</p>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}`);
+});
