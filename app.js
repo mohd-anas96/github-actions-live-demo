b0937c7647b29181905773e03dc177e485c01038
EC2 Default User <ec2-user@ip-172-31-8-13.ap-south-1.compute.internal>
2026-06-29 15:44:21 +0000

    Updated app

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send(`
         <h1>GitHub Actions CI/CD Demo</h1>
-        <h2>Version 1.0</h2>
+        <h2>Updated Version 2.0</h2>
         <p>Application Successfully Deployed using GitHub Actions</p>
     `);
 });
